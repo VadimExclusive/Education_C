9292cfaf68d4186c6c441dc59e1e661663bf92ab
Vadim <Berkut501136@gmail.com>
2022-10-11 08:31:46 +0300

    Lessons_1 project added

---
+++ Lessons/Lesson_1/Ex_002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "татьяна")
+{
+Console.WriteLine("Приветствую, моя госпожа ");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
