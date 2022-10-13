7fe3ec05b9ee4fbcebe4b3df30ea5470349331e6
Vadim <Berkut501136@gmail.com>
2022-10-13 14:38:45 +0300

    Ex_001_Seminar_2 added

--- Seminars/Seminar_2/ClassWork/Ex_001/Program.cs
+++ Seminars/Seminar_2/ClassWork/Ex_001/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Напишите программу, которая принимает двузначное число и показывает наибольшую цифру числа.
+
+int FineBiggerDigit(int num)
+{
+    int result;
+
+    if(num < 10 || num >= 100)
+        return -1;    
+    else
+    {
+        int ed = num % 10;
+        int dec = num / 10;
+
+        if(ed > dec)
+            result = ed;
+        else
+            result = dec;
+    }
+    return result;
+}
+
+Console.Write("Input a two-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int biggerDigit = FineBiggerDigit(number);
+Console.WriteLine($"Bigger digit of {number} is {biggerDigit}");
