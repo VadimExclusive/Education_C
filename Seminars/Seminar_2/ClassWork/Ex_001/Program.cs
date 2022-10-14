37d6315c3e787e145613b077a5113c57dcaf3ec0
Vadim <Berkut501136@gmail.com>
2022-10-14 13:22:47 +0300

    Ex_001_Seminar_2 added

--- Seminars/Seminar_2/ClassWork/Ex_001/Program.cs
+++ Seminars/Seminar_2/ClassWork/Ex_001/Program.cs
@@ -1,5 +1,6 @@
-﻿// Задача: Напишите программу, которая принимает двузначное число и показывает наибольшую цифру числа.
+﻿// Задача_1: Напишите программу, которая принимает двузначное число и показывает наибольшую цифру числа.
 
+/*
 int FineBiggerDigit(int num)
 {
     int result;
@@ -24,3 +25,36 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int biggerDigit = FineBiggerDigit(number);
 Console.WriteLine($"Bigger digit of {number} is {biggerDigit}");
+*/
+
+// Задача_2: Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+/*
+int randomThreeDigit = new Random().Next(100, 999);
+Console.WriteLine($"Random three-digit number is: {randomThreeDigit}");
+
+int RemovingTheSecondDigit(int num)
+{
+    int ed = (num / 100) % 10;
+    int hun = num % 10;
+    int result = ed * 10 + hun;
+    int secCat = (randomThreeDigit % 100) / 10;
+    Console.WriteLine($"Delete two category number is: {secCat}");
+    return result;
+}
+
+Console.WriteLine($"it turned out the number: {RemovingTheSecondDigit(randomThreeDigit)}");
+*/
+
+// Задача_3: Напишите программу, которая принимает на вход число "n" и проверяет, кратно ли оно одновременно "a" и "b" (цельночисленные делители, так же задаются пользователем).
+
+Console.WriteLine("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a divider A: ");
+int dividerA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a divider B: ");
+int dividerB = Convert.ToInt32(Console.ReadLine());
+
+
+
+
