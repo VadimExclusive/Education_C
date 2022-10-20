ed595c31bda01e7be1b468874d55d1680be9969a
Vadim <Berkut501136@gmail.com>
2022-10-20 20:36:08 +0300

    Lesson 1  Homework added

--- Lessons/Lesson_1/Ex_002/Program.cs
+++ Lessons/Lesson_1/Ex_002/Program.cs
@@ -1,11 +1,1 @@
-﻿Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
-if(username.ToLower() == "татьяна")
-{
-Console.WriteLine("Приветствую, моя госпожа ");
-}
-else
-{
-Console.Write("Привет, ");
-Console.WriteLine(username);
-}
+﻿
